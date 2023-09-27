c736a4b73011ce3308437fdc694b568eb62287f7
María <130462647+keroru24990@users.noreply.github.com>
2023-09-27 12:15:30 +0200

    MainActivity java y xml
    Añadir campos de texto como si fuesen un formulario y en el main recuperar los campos. Añadir un evento a un botón mediante la inyección de una clase.

--- 1AppAndroid/app/src/main/java/com/example/a1_appandroid/MainActivity.java
+++ 1AppAndroid/app/src/main/java/com/example/a1_appandroid/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.a1_appandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtUser;
+    private EditText edtPassword;
+    private Button btnSingIn;
 
+    /*CICLO DE VIDA : ONLOAD JAVASCRIPT*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //Carga el activity_main : index.html o main java
+
+        //document.getElementById("idUser");
+        edtUser = findViewById(R.id.edtUser); //Getelemementbyid android
+        edtPassword = findViewById(R.id.edtPassword);
+        btnSingIn = findViewById(R.id.btnSingIn);
+        //btnEnviar.addeventListener("click", "Bienvenido a JS");
+        btnSingIn.setOnClickListener(new View.OnClickListener() { //Instacia una clase anonima, clase que se crea desde el método
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
