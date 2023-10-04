084046226ca6155234b9256050fa9ae866ffd6d6
María <130462647+soodita@users.noreply.github.com>
2023-10-04 11:19:07 +0200

    04/10/2023

--- Logion0310/app/src/main/java/com/example/login0310/MainActivity.java
+++ Logion0310/app/src/main/java/com/example/login0310/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.login0310;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        innitComponents();
+
     }
+
+    private void innitComponents(){
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
 }
