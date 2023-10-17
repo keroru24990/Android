9e4cf3904d86b87a2bc2decdf08996985ea90135
María <130462647+soodita@users.noreply.github.com>
2023-10-17 10:37:50 +0200

    17/10

--- Logion0310/app/src/main/java/com/example/login0310/MainActivity.java
+++ Logion0310/app/src/main/java/com/example/login0310/MainActivity.java
@@ -6,17 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.login0310.Services.MainService;
+import com.example.login0310.Services.ViewMain;
+
+public class MainActivity extends AppCompatActivity implements ViewMain {
     private EditText edtEmail;
     private EditText edtPassword;
     private Button btnLogin;
+
+    private MainService mService = new MainService();
+
+
+    /*PATRON SINGLETON*/
+    private static MainActivity mainActivity = null;
+    public static MainActivity getInstance(){
+        return  mainActivity;
+    };
+    /*FIN PATRON SINGLETON*/
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainActivity= this;
         innitComponents();
 
     }
@@ -28,10 +44,14 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this, "Welcome to miami", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
 
+    @Override
+    public void show() {
+
+    }
 }
