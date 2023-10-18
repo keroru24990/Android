57a6de4c6842058352a585f334b1d1df785683dc
María <130462647+soodita@users.noreply.github.com>
2023-10-18 09:17:44 +0200

    [18/10/2023] Actualizado cambios profesor.
    Del día 17/10/2023

--- Logion0310/app/src/main/java/com/example/login0310/MainActivity.java
+++ Logion0310/app/src/main/java/com/example/login0310/MainActivity.java
@@ -6,32 +6,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.login0310.actions.ServicePeliculas;
+import com.example.login0310.actions.ViewPeliculas;
+
+
+public class MainActivity extends AppCompatActivity
+        implements ViewPeliculas {
     private EditText edtEmail;
     private EditText edtPassword;
     private Button btnLogin;
 
-
+    private ServicePeliculas sPeliculas =
+            new ServicePeliculas(this);
+    /* PATRÓN SINGLETON*/
+    private static MainActivity mainActivity = null;
+    public static MainActivity getInstance(){
+        return mainActivity; //0x457845AF
+    }
+    /* FIN PATRÓN SINGLETON*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        innitComponents();
-
+        mainActivity = this;
+        initComponents();
     }
-
-    private void innitComponents(){
+    private void initComponents(){
         edtEmail = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
         btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
     }
 
-
+    @Override
+    public void showPeliculas(String message) {
+        Toast.makeText(mainActivity, message, Toast.LENGTH_SHORT).show();
+    }
 }
---
+++ Logion0310/app/src/main/java/com/example/login0310/actions/ServicePeliculas.java
@@ -0,0 +1,23 @@
+package com.example.login0310.actions;
+
+public class ServicePeliculas {
+
+    private ViewPeliculas view;
+    public ServicePeliculas(ViewPeliculas view){
+        this.view = view;
+    }
+
+    void getDatosPeliculas(){
+        // Voy a por los datos
+        // VIAJOOOOOOOOO A POR LOS DATOS!!!!!!!!!!!!
+        // THREADS!!!!!!!!!!!!!!!!!!!!
+        // RETROFIT!!!!!!!!!!!!
+        // Toast.makeText(MainActivity.getInstance(), "Welcome To Miami!!!", Toast.LENGTH_SHORT).show();
+        view.showPeliculas("Los datos han ido bien: ['films': {''}]");
+
+        // fetch ("url=http://")-->
+        // json
+
+
+    }
+}
